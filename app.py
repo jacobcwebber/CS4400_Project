00b615749d42240d3f96986b17110fc7b2cf95d9
Jacob <jacobcwebber@gmail.com>
2017-10-15 20:49:26 -0400

    lots of new pages, updated CSS, and new routings

--- app.py
+++ app.py
@@ -12,16 +12,47 @@ app.config.update(
     TEMPLATES_AUTO_RELOAD=True,
     EXPLAIN_TEMPLATE_LOADING=True)
 
-#render login page
+#render login/registration page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#render user login page
+@app.route('/passenger')
+def passenger():
+    return render_template('passenger.html')
 
-#render admin login page
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
 
+@app.route('/admin/station-management')
+def station_management():
+    return render_template('station_management.html')
 
+@app.route('/admin/station-management/create-station')
+def create_station():
+    return render_template('create_station.html')
+
+@app.route('/admin/station-detail/1')
+def station_detail(id=1):
+    return render_template('station_detail.html', id=id)
+
+@app.route('/admin/suspended-cards')
+def suspended_cards():
+    return render_template('suspended_cards.html')
+
+@app.route('/admin/card-management')
+def card_management_admin():
+    return render_template('card_management_admin.html')
+
+@app.route('/admin/flow-report')
+def flow_report():
+    return render_template('flow_report.html')
+
+@app.route('/passenger/card-management')
+def card_management_passenger():
+    return render_template('card_management_passenger.html')
+
+@app.route('/passenger/trip-history')
+def trip_history():
+    return render_template('trip_history.html')
