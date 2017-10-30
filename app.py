092c4ea9b9d8ad148f256a1da491e66293a05c14
Jacob <jacobcwebber@gmail.com>
2017-10-30 11:20:27 -0400

    updating app.py considerably

--- app.py
+++ app.py
@@ -1,21 +1,103 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, logging, session, flash, redirect
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from functools import wraps
+
 app = Flask(__name__)
 
-#To start local server:
-#1) In command line go to directory with project
-#2) Type FLASK_APP=app.py
-#3) For debugger mode type FLASK_DEBUG=1 (this auto-reloads the page upon code changes)
-#4) Type flask run
-#5) Copy resulting IP address into url bar
+# MySQL configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'marta_app'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-app.config.update(
-    TEMPLATES_AUTO_RELOAD=True,
-    EXPLAIN_TEMPLATE_LOADING=True)
+#initialize MySQL
+mysql = MySQL(app)
 
-#render login/registration page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(username, email, password) VALUES (%s, %s, %s)", (username, email, password))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Congratulations! You are now registered.', 'success')
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_attempt = request.form['password']
+
+        cur = mysql.connection.cursor()
+
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+
+            if sha256_crypt.verify(password_attempt, password):
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in.', 'success')
+                return redirect(url_for('dashboard'))
+
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            cur.close()
+
+        else:
+            error = "Username not found"
+            return render_template('login.html', error=error)
+
+    render_template('login.html')
+
+# TODO: Figure out how this works more
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash ('Unauthorized, please login.', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
 
 @app.route('/passenger')
 def passenger():
@@ -33,8 +115,11 @@ def station_management():
 def create_station():
     return render_template('create_station.html')
 
-@app.route('/admin/station-detail/1')
-def station_detail(id=1):
+@app.route('/admin/station-detail/<string:id>/')
+def station_detail(id):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM stations WHERE id = %s", [id])
+    station = result.fetchone()
     return render_template('station_detail.html', id=id)
 
 @app.route('/admin/suspended-cards')
@@ -56,3 +141,6 @@ def card_management_passenger():
 @app.route('/passenger/trip-history')
 def trip_history():
     return render_template('trip_history.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
